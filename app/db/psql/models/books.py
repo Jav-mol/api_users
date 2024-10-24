bc1584d978d6e4b861db5cab3281ac6180c47e45
Javi <Jav.molh@gmail.com>
2024-10-24 00:00:11 -0300

    books

--- app/db/psql/models/books.py
+++ app/db/psql/models/books.py
@@ -1,1 +1,11 @@
-from sqlalchemy import MetaData
+from sqlalchemy import MetaData, Table, Column, Integer, String, DateTime
+from datetime import datetime
+
+metadata = MetaData()
+
+books = Table('books', metadata,
+              Column('Id', Integer, primary_key=True),
+              Column('Title', String, unique=True),
+              Column('Author', String, unique=True),
+              Column("Created_at", DateTime, default=datetime.now().strftime("%Y-%m-%d %H:%M:%S")))
+
