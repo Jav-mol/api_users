38f303557400705b40603939aa6dcd11da5641c4
Javi <Jav.molh@gmail.com>
2024-10-29 11:09:48 -0300

    read books

--- app/crud/books.py
+++ app/crud/books.py
@@ -2,14 +2,14 @@ from sqlalchemy import Connection
 from schemas.books import Book
 from db.psql.models.books import books
 
-def check_book_exists(db: Connection, book: Book) -> dict:
+def check_book_exists(db: Connection, book: Book) -> bool:
     book_db = db.execute(books.select().where(books.c.title == book.title, books.c.author == book.author))
     return bool(book_db.all())
     
-def insert_book_db(db: Connection, book: Book):
+def insert_book_db(db: Connection, book: Book) -> int:
     book_inserted = db.execute(books.insert().values(title=book.title, author=book.author))
-    return book_inserted.inserted_primary_key
+    return book_inserted.inserted_primary_key[0]
 
-def read_books_db(db: Connection):
+def read_books_db(db: Connection) -> list[dict]:
     books_db = db.execute(books.select())
     return [book for book in books_db] 
--- app/tests/unit/crud/test_books_crud.py
+++ app/tests/unit/crud/test_books_crud.py
@@ -32,13 +32,25 @@ def list_books():
 
 
 def test_check_book_exists(connection: Connection, list_books: list):
-    for book in list_books:
-        insert_book_db(db=connection, book=Book(**book))
+    insert_book_db(db=connection, book=Book(**list_books[0]))
     
     book_exist = check_book_exists(db=connection, book=Book(title="Los Miserables", author="Victor Hugo"))
-    print(book_exist)
+    assert book_exist == True
+    
+    book_exist_2 = check_book_exists(db=connection, book=Book(title="Los Miserables2", author="Victor Hugo"))
+    assert book_exist_2 == False
+    
     
 def test_insert_book_db(connection: Connection, list_books: list):
     book = Book(**list_books[0])
-    book_db = insert_book_db(db=connection, book=book)
-    print(book_db[0])
+    book_id = insert_book_db(db=connection, book=book)
+    assert book_id == 1
+
+
+def test_read_books_db(connection: Connection, list_books: list):
+    for book in list_books:
+        insert_book_db(db=connection, book=Book(**book))
+    
+    books_db = read_books_db(db=connection)
+    assert len(books_db) == 10
+    
