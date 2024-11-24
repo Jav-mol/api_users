c91fbeab7ab3c71b03ae5eae887b995e0832dd39
Javi <Jav.molh@gmail.com>
2024-11-24 20:16:17 -0300

    diagram

--- app/tests/unit/api/routers/test_users_router.py
+++ app/tests/unit/api/routers/test_users_router.py
@@ -1,6 +1,8 @@
 from fastapi.testclient import TestClient
 from fastapi import APIRouter, Depends, FastAPI
 
+from api.routers.users import router
+
 from pymongo.collection import Collection
 from schemas.users import UserCreate, UserDB, UserOutput
 from services.users_services import insert_user_db
@@ -9,6 +11,7 @@ from db.mongodb.get_db import get_db_mongo, get_db_mongo_override
 import pytest
 
 app = FastAPI()
+app.include_router(router)
 
 client = TestClient(app)
 
@@ -18,7 +21,7 @@ app.dependency_overrides[get_db_mongo] = get_db_mongo_override
 def test_user():
     user = UserCreate(username="Javi", password="1234", email="Jav@gmail.com")
     
-    return user.model_dump()
+    return user
 
 
 def test_create_user(test_user):
