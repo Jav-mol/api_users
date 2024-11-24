735a6bea658dad5be35e895e3fbca4db61f51f90
Javi <Jav.molh@gmail.com>
2024-11-24 19:27:51 -0300

    dependecies override

--- app/tests/unit/api/routers/test_users_router.py
+++ app/tests/unit/api/routers/test_users_router.py
@@ -1,7 +1,5 @@
 from fastapi.testclient import TestClient
-from fastapi import APIRouter, Depends
-
-from api.routers.users import router
+from fastapi import APIRouter, Depends, FastAPI
 
 from pymongo.collection import Collection
 from schemas.users import UserCreate, UserDB, UserOutput
@@ -10,11 +8,11 @@ from db.mongodb.get_db import get_db_mongo, get_db_mongo_override
 
 import pytest
 
-client = TestClient(router)
+app = FastAPI()
 
-@router.dependencies[Depends(get_db_mongo)]
-def override_db():
-    return get_db_mongo_override()
+client = TestClient(app)
+
+app.dependency_overrides[get_db_mongo] = get_db_mongo_override
 
 @pytest.fixture
 def test_user():
