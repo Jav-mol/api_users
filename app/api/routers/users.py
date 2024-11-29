88fbe60cb6de56f0be52dd1375901502bc7214e0
Javi <Jav.molh@gmail.com>
2024-11-29 19:46:54 -0300

    test

--- app/api/routers/users.py
+++ app/api/routers/users.py
@@ -2,9 +2,9 @@ from fastapi import APIRouter, Depends
 from typing import Annotated
 from pymongo.collection import Collection
 from schemas.users import UserCreate, UserDB, UserOutput
-from services.users_services import insert_user_db
 from db.mongodb.get_db import get_db_mongo_override, get_db_mongo
 from db.psql.get_db import get_db_psql
+from services.users_services import create_user
 
 router = APIRouter(
     prefix="/users"
@@ -12,4 +12,5 @@ router = APIRouter(
 
 @router.post("")
 async def create_user(user: UserCreate, db: Annotated[Collection, Depends(get_db_mongo)]):
-    return user
+    #user_created = create_user(user=user, db=db)
+    return user #_created
--- app/tests/unit/api/routers/test_users_router.py
+++ app/tests/unit/api/routers/test_users_router.py
@@ -1,6 +1,8 @@
 from fastapi.testclient import TestClient
 from fastapi import APIRouter, Depends, FastAPI
 
+from httpx import AsyncClient
+
 from api.routers.users import router
 
 from pymongo.collection import Collection
@@ -8,24 +10,28 @@ from schemas.users import UserCreate, UserDB, UserOutput
 from services.users_services import insert_user_db
 from db.mongodb.get_db import get_db_mongo, get_db_mongo_override
 
+from httpx import ASGITransport, AsyncClient
+
 import pytest
 
 app = FastAPI()
 app.include_router(router)
 
 client = TestClient(app)
-
 app.dependency_overrides[get_db_mongo] = get_db_mongo_override
+
 
 @pytest.fixture
 def test_user():
     user = UserCreate(username="Javi", password="1234", email="Jav@gmail.com")
+    return user.model_dump()
+
+@pytest.mark.anyio
+async def test_create_user(test_user):
+    #response = await client.post("/users", json=test_user)
+
+    async with AsyncClient(
+        transport=ASGITransport(app=app), base_url="http://test") as ac:
+        response = await ac.post("/users", json=test_user)
     
-    return user
-
-
-def test_create_user(test_user):
-    
-    response = client.post("/users", json=test_user)
-
     print(response.json())
